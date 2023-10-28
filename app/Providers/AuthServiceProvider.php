80ee9cdc485228b84d7acad9185f73d18aab4876
ahmadjundan <ahjundan160@gmail.com>
2023-10-28 20:18:58 +0700

    Role,Update profile&pass,list user,search,paginate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,10 +3,18 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
+
+    public static $permission = [
+        'dashboard' => ['superadmin', 'admin'],
+        'user' => ['superadmin'],
+    ];
     /**
      * The model to policy mappings for the application.
      *
@@ -21,6 +29,13 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        foreach(self::$permission as $feature => $roles) {
+            Gate::define($feature,  function (User $user) use ($roles) {
+                if (in_array($user->roles, $roles)) {
+                    return true;
+                }
+            });
+        }
+
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,12 +11,26 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory(5)->create();
+        User::factory(20)->create();
+
         User::create([
             'name' => 'Ahmad Jundan Sulthoni',
             'email' => 'jundan@gmail.com',
+            'phone' => '085123456789',
+            'bio' => 'Mahasiswa Teknik Informatika',
             'email_verified_at' => now(),
+            'roles' => 'admin',
             'password' => Hash::make('ahmadjundan'),
+        ]);
+
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'super@gmail.com',
+            'phone' => '085123456789',
+            'bio' => 'Somebody',
+            'email_verified_at' => now(),
+            'roles' => 'superadmin',
+            'password' => Hash::make('superadmin'),
         ]);
     }
 }
